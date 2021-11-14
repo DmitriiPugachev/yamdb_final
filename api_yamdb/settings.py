c32d0ff54544337f9966701a5ada570a0e169aac
DmitriiPugachev <pugachev.feddebate@gmail.com>
2021-11-14 13:20:38 +0100

    DEBUG changing with variable added.

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv("SECRET_KEY", default="default_value")
 
-DEBUG = False
+DEBUG = os.getenv("DEBUG", default=False)
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOST", default="localhost").split()
 AUTH_USER_MODEL = "reviews.User"
